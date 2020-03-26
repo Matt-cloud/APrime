693bd8fa4e029e8ee1096a3b79e13629ecacac0e
Matthew Dev <philmattdev@gmail.com>
2020-03-26 11:38:52 +0800

    Added cooldowns on chatbot per guild

--- cogs/chatbot.py
+++ cogs/chatbot.py
@@ -15,18 +15,22 @@ class ChatBotHandler(commands.Cog):
                 'chatterbot.logic.BestMatch'
             ]
         )
+        self.cooldowns = {}
+        self.cooldownLimit = 2 # You have to wait for 5 seconds before chatting with the chatbot again
     
     async def getResponse(self, content):
         return self.chatbot.get_response(content)
     
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.guild:
-            channel_id, guild_id = message.channel.id, message.guild.id
-            server_prefix = await bot.getPrefix(message.guild, db)
 
-            if message.author.bot:
-                return # Ignore all bots
+        if message.author.bot:
+            return # Ignore all bots
+
+        if message.guild:
+
+            channel_id, guild_id = message.channel.id, message.guild.id # ??? why
+            server_prefix = await bot.getPrefix(message.guild, db)
             
             if message.content == "" or message.content is None:
                 return # Ignore empty messages
@@ -35,6 +39,21 @@ class ChatBotHandler(commands.Cog):
                 return
 
             if await db.chatbots.count_documents({"guild_id": guild_id, "channel_id": channel_id}):
+
+                if message.guild.id in self.cooldowns: # Cooldown per guild
+                    stamp = self.cooldowns[message.guild.id]
+                    if int(time.time()) - stamp < self.cooldownLimit:
+                        waitingTime = self.cooldownLimit - (int(time.time()) - stamp)
+                        s = ""
+
+                        if waitingTime > 1:
+                            s = "s"
+
+                        return await message.channel.send(f"Please wait `{waitingTime} second{s}` before chatting with me again.")
+                    self.cooldowns.pop(message.guild.id)
+                else:
+                    self.cooldowns[message.guild.id] = int(time.time())
+
                 content = message.content
                 response = await self.bot.loop.create_task(self.getResponse(content))
                 
